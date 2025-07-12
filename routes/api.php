a4849627861d989209916a33d59551a542e0e306
Jan Eris Saludo <janerissaludo@gmail.com>
2025-07-13 01:21:43 +0800

    fix: Update UserController namespace to include API prefix in routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Domains\Auth\Controllers\UserController;
+use App\Domains\Auth\Controllers\API\UserController;
 
 Route::prefix('api')->group(function () {
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
